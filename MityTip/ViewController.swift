d1ecdeb0a8dfe2834b1d8914eec7739c570813ca
Tamyia Gill <Tamyiagill20@gmail.com>
2019-09-10 11:46:41 -0500

    Initial Commit

---
+++ MityTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MityTip
+//
+//  Created by cs_apple_03 on 9/10/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MityTipTests/MityTipTests.swift
@@ -0,0 +1,34 @@
+//
+//  MityTipTests.swift
+//  MityTipTests
+//
+//  Created by cs_apple_03 on 9/10/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import XCTest
+@testable import MityTip
+
+class MityTipTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
